1b1040afc6994c6b91b4a838e02d26d4ecb8418c
Nikilhari <nikilhari3@gmail.com>
2025-01-03 18:16:58 +0530

    changes on UI

--- frontend/src/components/Navbar_logout/Navbar_logout.jsx
+++ frontend/src/components/Navbar_logout/Navbar_logout.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import styles from './Navbar_logout.module.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import styles from "../Navbar/Navbar.module.css";
+import { Link } from "react-router-dom";
 const Navbar_logout = () => {
-    return (
-        <div className={styles.c}>
-            <div className={styles.logo}>
-                <h1>WHISPER WIRE</h1>
-            </div>
-            <div className={styles.enclose}>
-                <Link to='/' className={styles.contents} onClick={() => {
-                    localStorage.removeItem('token');
-                }}>
-                    <h3>Logout</h3>
-                </Link>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.c}>
+      <div className={styles.logo}>
+        <h1>WHISPER WIRE</h1>
+      </div>
+      <div className={styles.enclose}>
+        <Link
+          to="/"
+          className={styles.contents}
+          onClick={() => {
+            localStorage.removeItem("token");
+          }}
+        >
+          <h3>Logout</h3>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar_logout
+export default Navbar_logout;
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,68 +1,73 @@
-import React from 'react'
-import styles from './Login.module.css'
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import Navbar from '../../components/Navbar/Navbar';
-import axios from 'axios'
-import { useState } from 'react';
+import React from "react";
+import styles from "./Login.module.css";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import axios from "axios";
+import { useState } from "react";
 const Login = () => {
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState(false);
-    const handleSubmit = async (event) => {
-        setLoading(true);
-        event.preventDefault();
-        const rollNo = document.getElementById("rollNumber");
-        const password = document.getElementById("password")
-        const values = {
-            rollNumber: rollNo.value,
-            password: password.value
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async (event) => {
+    setLoading(true);
+    event.preventDefault();
+    const rollNo = document.getElementById("rollNumber");
+    const password = document.getElementById("password");
+    const values = {
+      rollNumber: rollNo.value,
+      password: password.value,
+    };
+    await axios
+      .post("https://anonymous-feed-back-app-1.onrender.com/auth/login", values)
+      .then((res) => {
+        setLoading(false);
+        if (res.data === "user_not_found") {
+          alert("Enter valid username/password");
+        } else if (res.data === "password_do_not_match") {
+          alert("Enter valid usermame/password");
+        } else {
+          localStorage.setItem("token", res.data);
+          navigate("/Feedback");
         }
-        await axios.post("https://anonymous-feed-back-app-1.onrender.com/auth/login", values).then((res) => {
-            setLoading(false)
-            if (res.data === "user_not_found") {
-                alert("Enter valid username/password");
-            }
-            else if (res.data === "password_do_not_match") {
-                alert("Enter valid usermame/password");
-            }
-            else {
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <>
+      <Navbar />
+      {loading ? (
+        <div className="loader"></div>
+      ) : (
+        <div className={styles.wrapper}>
+          <div className={styles.title}>Login Form</div>
+          <form className={styles.enclose} onSubmit={handleSubmit}>
+            <div className={styles.field}>
+              <input
+                type="text"
+                required
+                id="rollNumber"
+                pattern="[2-9]{2}[A-Z]{3}[0-9]{3}"
+                title="Roll number should be in format 22ABC132"
+              />
+              <label>Roll No</label>
+            </div>
+            <div className={styles.field}>
+              <input type="password" required id="password" />
+              <label>Password</label>
+            </div>
+            <div className={styles.field}>
+              <input type="submit" value="Login" />
+            </div>
+            <div className={styles.signLink}>
+              Not a member? <Link to="/Register">Register</Link>
+            </div>
+          </form>
+        </div>
+      )}
+    </>
+  );
+};
 
-                localStorage.setItem("token", res.data);
-                navigate('/Feedback')
-            }
-        }).catch((err) => {
-            console.log(err)
-        })
-    }
-    return (
-        <>
-            <Navbar />
-            {loading ? <div className='loader'>
-            </div> :
-                <div className={styles.wrapper}>
-                    <div className={styles.title}>
-                        Login Form
-                    </div>
-                    <form className={styles.enclose} onSubmit={handleSubmit}>
-                        <div className={styles.field}>
-                            <input type="text" required id="rollNumber" pattern='[2-9]{2}[A-Z]{3}[0-9]{3}' title='Roll number should be in format 22ABC132' />
-                            <label>Roll No</label>
-                        </div>
-                        <div className={styles.field}>
-                            <input type="password" required id="password" />
-                            <label>Password</label>
-                        </div>
-                        <div className={styles.field}>
-                            <input type="submit" value="Login" />
-                        </div>
-                        <div className={styles.signLink}>
-                            Not a member? <Link to='/Register'>Register</Link>
-                        </div>
-                    </form>
-                </div>
-            }
-        </>
-    )
-}
-
-export default Login
+export default Login;
