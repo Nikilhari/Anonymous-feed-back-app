6a7d748403a60c0736b44ba0cc4d199ef140dcb2
Nikilhari <nikilhari3@gmail.com>
2025-08-01 12:30:28 +0530

    running frontend service using proper security groups

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
