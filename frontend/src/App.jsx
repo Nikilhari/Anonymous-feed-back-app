345120cfb9bf8a090ed91a463e89a53ef5504c62
Nikilhari <nikilhari3@gmail.com>
2025-08-02 09:05:09 +0530

    learning--1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,18 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Register from './pages/Register/Register'
-import Login from './pages/Login/Login'
-import Feedback from './pages/Feedback/Feedback';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import Feedback from "./pages/Feedback/Feedback";
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path={'/'} element={<Login />} />
-        <Route path={'/Register'} element={<Register />} />
-        <Route path={'/Feedback'} element={<Feedback />} />
+        <Route path={"/"} element={<Login />} />
+        <Route path={"/Register"} element={<Register />} />
+        <Route path={"/Feedback"} element={<Feedback />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-
-
-
-export default App
+export default App;
